6932f0fbb44fe54deffa9c5c4757cf3ae3407549
Gabriel de Diego <gdedi651@gmail.com>
2017-09-24 14:24:27 -0400

    fix: remove unused Update function from trampoline script
    remove Update overhead from unused function(s)

--- Assets/PerformanceBounceback/Scripts/Trampoline.cs
+++ Assets/PerformanceBounceback/Scripts/Trampoline.cs
@@ -9,14 +9,10 @@ public class Trampoline : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
         scoreScript = GameObject.Find("GameManager").GetComponent<GameManager>();
         pSystem = GetComponentInChildren<ParticleSystem>();
-	}
+    }
+	
 
     void OnCollisionEnter(Collision col)
     {
